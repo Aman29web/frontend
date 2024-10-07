0f8b7dc18ce0f6143d9ed94074b64d47e22758c9
Aman29web <amansinghvns29@gmail.com>
2024-10-07 18:08:31 +0530

    ok

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import './CartItems.css'
+import { ShopContext } from '../../Context/ShopContext'
+
+const CartItems = () => {
+  
+    const {all_product,cartItems,removeFromCart}=useContext(ShopContext);
+    
+  return (
+    <div className='cartitems'>
+      <div className="cartitms-format-main"></div>
+    </div>
+  )
+}
+
+export default CartItems
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,13 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import React from "react";
 import all_product from '../Components/Assests/all_product'
 
 export const ShopContext =createContext(null);
+
+const getDefaultCart =()=>{
+    let cart={};
+    for (let index=0;index<all_product.length+1;index++){
+        cart[index]=0;
+    }
+    return cart;
+}
+
 const ShopConextProvider=(props)=>{
 
-    const conextValue={all_product};
-
+    const [cartItems,setCartItems]=useState(getDefaultCart());
+   
     
+    const addToCart =(itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
+    }
+
+    const removeFromCart =(itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    const conextValue={all_product,cartItems,addToCart,removeFromCart};
+
+
     return (
         <ShopContext.Provider value={conextValue}>
 
